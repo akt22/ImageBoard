f7ec8d8c427768f0f8f0276d9f07b5e608addd3c
Akita Ryo <akita@ai.cs.kobe-u.ac.jp>
2016-02-01 14:35:24 +0900

    add helper

--- app/helpers/application_helper.rb
+++ app/helpers/application_helper.rb
@@ -1,2 +1,12 @@
 module ApplicationHelper
+
+  # ページごとのタイトルを返す
+  def full_title(page_title='')
+    base_tile = "ImageBoard"
+    if page_title.empty?
+      base_tile
+    else
+      page_title + " | " + base_tile
+    end
+  end
 end
