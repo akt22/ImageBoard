9b3bc514d9df65df05830f75e63e4b865854d463
Akita Ryo <akita@ai.cs.kobe-u.ac.jp>
2016-02-02 19:25:13 +0900

    11.1.2 passed

--- test/models/micropost_test.rb
+++ test/models/micropost_test.rb
@@ -4,6 +4,7 @@ class MicropostTest < ActiveSupport::TestCase
 
   def setup
     @user = users(:michael)
+    # 次の行は慣習的に間違っている
     @micropost = Micropost.new(content: "Lorem ipsum", user_id: @user.id)
   end
 
@@ -12,8 +13,17 @@ class MicropostTest < ActiveSupport::TestCase
   end
 
   test "user id should be present" do
-    @micropost.user_id.valid?
+    @micropost.user_id = nil
     assert_not @micropost.valid?
   end
 
+  test "content should be present" do
+    @micropost.content = " "
+    assert_not @micropost.valid?
+  end
+
+  test "content should be at most 140 characters" do
+    @micropost.content = "a" * 141
+    assert_not @micropost.valid?
+  end
 end
