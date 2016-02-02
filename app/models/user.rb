856e3c6deb30cc0b142e44a094896e4abac34991
Akita Ryo <akita@ai.cs.kobe-u.ac.jp>
2016-02-02 22:48:08 +0900

    finish chapter 11

--- app/models/user.rb
+++ app/models/user.rb
@@ -1,4 +1,5 @@
 class User < ActiveRecord::Base
+  has_many :microposts, dependent: :destroy
   attr_accessor :remember_token, :reset_token
   before_save { self.email = email.downcase }
   before_save { email.downcase! }
@@ -47,5 +48,9 @@ class User < ActiveRecord::Base
 
   def password_reset_expired?
     reset_sent_at < 2.hours.ago
+  end
+
+  def feed
+    Micropost.where("user_id = ?", id)
   end
 end
