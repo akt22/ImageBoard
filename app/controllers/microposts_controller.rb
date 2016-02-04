e12c212f0dd48d02578729b1a24970aae1fb1fff
Akita Ryo <akita@ai.cs.kobe-u.ac.jp>
2016-02-05 02:13:35 +0900

    reach vision

--- app/controllers/microposts_controller.rb
+++ app/controllers/microposts_controller.rb
@@ -1,11 +1,14 @@
 class MicropostsController < ApplicationController
-  before_action :logged_in_user, only: [:create, :destroy]
+  before_action :logged_in_user, only: [:create, :destroy, :upload]
   before_action :correct_user, only: :destroy
 
   def create
     @micropost = current_user.microposts.build(micropost_params)
     if @micropost.save
       flash[:success] = "Micropost created"
+      if @micropost.picture?
+        p vision @micropost.picture.filename
+      end
       redirect_to root_url
     else
       @feed_items = current_user.feed.paginate(page: params[:page])
@@ -25,9 +28,27 @@ class MicropostsController < ApplicationController
       params.require(:micropost).permit(:picture_cache, :content, :picture)
     end
 
-     def correct_user
+    def correct_user
       @micropost = current_user.microposts.find_by(id: params[:id])
       redirect_to root_url if @micropost.nil?
+    end
+
+    def vision(file_path)
+      http_client = HTTPClient.new
+      endpoint_uri = 'http://www.ai.cs.kobe-u.ac.jp/vision'
+      content = Base64.strict_encode64(File.new(file_path, 'rb').   read)
+      response = http_client.post_content(endpoint_uri,     request_json(content), 'Content-Type' => 'application/json'  )
+      result_parse(response)
+    end
+
+    def request_json(content)
+      {
+        requests: [{
+          img: {
+            content: content
+          }
+        }]
+      }.to_json
     end
 
 end
