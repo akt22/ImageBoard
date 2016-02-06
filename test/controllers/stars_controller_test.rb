6918276d599522fff80bae29ecb0a43a54f44920
Akita Ryo <akita@ai.cs.kobe-u.ac.jp>
2016-02-06 22:15:59 +0900

    add integration test

--- test/controllers/stars_controller_test.rb
+++ test/controllers/stars_controller_test.rb
@@ -8,13 +8,6 @@ class StarsControllerTest < ActionController::TestCase
     @star.save
   end
 
-  # test "should unstar" do
-  #   Rails::logger.debug "STAR_ID: #{@star.id}"
-  #   assert_difference 'Star.count', -1 do
-  #     delete star_path(@star)
-  #   end
-  # end
-
   test "should redirect star when not logged in" do
     assert_no_difference 'Star.count' do
       post :create, { micropost_id: @micropost.id }
--- test/integration/staring_test.rb
+++ test/integration/staring_test.rb
@@ -36,10 +36,18 @@ class StaringTest < ActionDispatch::IntegrationTest
     log_in_as(@user)
     star = @micropost.stars.build(user_id: @user.id, micropost_id: @micropost.id)
     star.save
-    get root_path
     assert_difference 'Star.count', -1 do
       xhr :delete, star_path(star)
     end
   end
 
+  test "should count decrease when micropost is deleted" do
+    log_in_as(@user)
+    star = @micropost.stars.build(user_id: @user.id, micropost_id: @micropost.id)
+    star.save
+    num_star = @micropost.stars.count
+    assert_difference 'Micropost.count', -1 * num_star do
+      delete micropost_path(@micropost)
+    end
+  end
 end
