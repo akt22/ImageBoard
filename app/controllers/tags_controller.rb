aa3a472a02d3ea35a2e8af23a8d2f21d18a3b2ad
Akita Ryo <akita@ai.cs.kobe-u.ac.jp>
2016-02-07 14:11:14 +0900

    fix img search

--- app/controllers/tags_controller.rb
+++ app/controllers/tags_controller.rb
@@ -1,6 +1,10 @@
 class TagsController < ApplicationController
   def index
-    @search = Tag.search(params[:q])
+    if params[:q].present?
+      @search = Tag.search(params[:q])
+    else
+      @search = Tag.search({tag_cont: "例. mountain"})
+    end
     @res_micropost = @search.result.paginate(page: params[:page])
   end
 end
