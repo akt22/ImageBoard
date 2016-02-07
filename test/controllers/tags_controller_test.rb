6263f7b5bb284636a05b82e090dc9754799f4771
Akita Ryo <akita@ai.cs.kobe-u.ac.jp>
2016-02-07 13:40:14 +0900

    test passed

--- test/controllers/tags_controller_test.rb
+++ test/controllers/tags_controller_test.rb
@@ -1,6 +1,13 @@
 require 'test_helper'
 
 class TagsControllerTest < ActionController::TestCase
+
+  def setup
+    @micropost = microposts(:orange)
+    @tag = @micropost.tags.build(tag: "label")
+    @tag.save
+  end
+
   test "should get index" do
     get :index
     assert_response :success
