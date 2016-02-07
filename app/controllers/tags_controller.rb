3ecc72e4366cd5b85e3f3f9ef98d5ebe1e1b6c16
Akita Ryo <akita@ai.cs.kobe-u.ac.jp>
2016-02-07 14:20:27 +0900

    fix img search

--- app/controllers/tags_controller.rb
+++ app/controllers/tags_controller.rb
@@ -1,7 +1,11 @@
 class TagsController < ApplicationController
   def index
     if params[:q].present?
-      @search = Tag.search(params[:q])
+      if params[:q][:tag_cont].blank?
+        @search = Tag.search({tag_cont: "例. mountain"})
+      else
+        @search = Tag.search(params[:q])
+      end
     else
       @search = Tag.search({tag_cont: "例. mountain"})
     end
